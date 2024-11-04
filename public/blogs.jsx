56f1f4da00d95e34de2c6dac922444d26a782e7f
shamssaniat <shams.saniat@northsouth.edu>
2024-11-04 15:05:25 +0600

    created Blogs and created fake data

---
+++ public/blogs.jsx
@@ -0,0 +1,53 @@
+[
+    {
+      "id": 1,
+      "cover": "https://i.ibb.co/mtKdNQ7/7489233.jpg",
+      "title": "Exploring the Future of AI",
+      "author": "Alex Johnson",
+      "author_img": "https://example.com/author1.jpg",
+      "posted_date": "2023-11-01",
+      "reading_time": "5",
+      "hashtag": ["#AI", "#Technology", "#Future"]
+    },
+    {
+      "id": 2,
+      "cover": "https://i.ibb.co/mtKdNQ7/7489233.jpg",
+      "title": "The Art of Minimalist Living",
+      "author": "Jamie Lee",
+      "author_img": "https://example.com/author2.jpg",
+      "posted_date": "2023-10-28",
+      "reading_time": "8",
+      "hashtag": ["#Minimalism", "#Lifestyle", "#Wellness"]
+    },
+    {
+      "id": 3,
+      "cover": "https://i.ibb.co/mtKdNQ7/7489233.jpg",
+      "title": "Understanding Quantum Computing",
+      "author": "Maria Lopez",
+      "author_img": "https://example.com/author3.jpg",
+      "posted_date": "2023-11-03",
+      "reading_time": "12",
+      "hashtag": ["#QuantumComputing", "#Science", "#Innovation"]
+    },
+    {
+      "id": 4,
+      "cover": "https://i.ibb.co/mtKdNQ7/7489233.jpg",
+      "title": "A Guide to Sustainable Travel",
+      "author": "Chris Tan",
+      "author_img": "https://example.com/author4.jpg",
+      "posted_date": "2023-11-02",
+      "reading_time": "7",
+      "hashtag": ["#Sustainability", "#Travel", "#EcoFriendly"]
+    },
+    {
+      "id": 5,
+      "cover": "https://i.ibb.co/mtKdNQ7/7489233.jpg",
+      "title": "Mastering the Basics of Python",
+      "author": "Taylor Kim",
+      "author_img": "https://example.com/author5.jpg",
+      "posted_date": "2023-10-30",
+      "reading_time": "6",
+      "hashtag": ["#Python", "#Coding", "#Programming"]
+    }
+  ]
+  
