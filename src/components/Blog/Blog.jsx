bde2d24e8e39d2d212c2aea3ea7bbfb81a54e380
shamssaniat <shams.saniat@northsouth.edu>
2024-11-05 06:44:01 +0600

    added display blog data and hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,33 @@
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+    const { title, cover, author, author_img, reading_time, posted_date, hashtag } = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl '>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtag.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>            
         </div>
     );
 };
 
-Blog.PropTypes = {
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
 }
 
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
-import React, { useState } from 'react';
+import { useState } from "react";
+import Blog from "../Blog/Blog";
+
+
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -9,9 +12,16 @@ const Blogs = () => {
         .then(res => res.json())
         .then(data => setBlogs(data))
     },[])
+    
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
+                ></Blog>)
+            }
         </div>
     );
 };
